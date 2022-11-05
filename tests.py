14aa4857fe8310708f6bc77610cc604f9bcafe7f
Raj <raj@SEONGJINWOOs-MacBook-Pro.local>
2022-11-04 23:30:12 -0700

    added 2 testst

--- tests.py
+++ tests.py
@@ -1,18 +1,32 @@
-from unicodedata import name
 import unittest
 import logic
 
 
 class TestLogic(unittest.TestCase):
 
-    def test_get_winner(self):
+    def test_get_winner_X(self):
         board = [
             ['X', None, 'O'],
             [None, 'X', None],
             [None, 'O', 'X'],
         ]
-        print('Test')
         self.assertEqual(logic.check_win(board, 'X'), 'X')
+    
+    def test_get_winner_O(self):
+        board = [
+            ['X', 'X', 'O'],
+            [None, 'X', 'O'],
+            ['X', 'O', 'O'],
+        ]
+        self.assertEqual(logic.check_win(board, 'O'), 'O')
+    
+    def test_draw(self):
+        board = [
+            ['X', 'O', 'O'],
+            ['O', 'X', 'X'],
+            ['X', 'O', 'O'],
+        ]
+        self.assertEqual(logic.check_win(board, 'X'), None)
 
 if __name__ == '__main__':
     unittest.main()
